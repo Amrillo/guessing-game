b266b34cd9c7bf9200514318ad8285a487404d05
Amrillo <amrishka88@gmail.com>
2024-10-18 11:17:48 +0500

    fix: delete empty spaces

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -4,22 +4,17 @@ class GuessingGame {
         this.min = 0;  
         this.max = 0 ; 
     }
-
-
     setRange(min, max) {
         this.min = min;  
         this.max = max; 
     }
-
     guess() {
         this.mid = Math.ceil((this.min + this.max)/2);
         return this.mid;
     }
-
     lower() {
       this.max = this.guess() - 1; 
     }
-
     greater() {
        this.min = this.guess() + 1;
     }
